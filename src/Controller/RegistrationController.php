48ca3960ad920f9c52b45b12b1a30fc57003ac3f
KevinVigne <kev.vigne@gmail.com>
2025-10-30 11:25:31 +0100

    Ajout de la fonctionnalité Se Souvenir de moi

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,8 +28,11 @@ class RegistrationController extends AbstractController
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
+            //$user->setRoles(['ROLE_ADMIN']); 
             $entityManager->persist($user);
             $entityManager->flush();
+            
+            
 
             // do anything else you need here, like send an email
 
